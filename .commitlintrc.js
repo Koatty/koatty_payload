eb6726cc5e4cacc53bcb16db255ea9078f10c217
linyyyang <linyyyang@tencent.com>
2022-02-19 00:16:47 +0800

    🐳 chore: husky

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,9 @@
 /*
- * 
+ * @Description: 
+ * @Usage: 
+ * @Author: richen
+ * @Date: 2021-12-18 20:53:46
+ * @LastEditTime: 2022-02-18 11:43:45
  */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
@@ -9,6 +13,6 @@ module.exports = {
     'scope-enum': [0], // 不校验scope类型
     'scope-empty': [0], // 不校验scope是否设置
     'subject-case': [0], // 不校验描述的字符格式
-    'subject-min-length': [2, 'always', 5], // 描述至少5个字符
+    'subject-min-length': [2, 'always', 2], // 描述至少2个字符
   },
 };
